0cb189eaacba88b15d85408b0e2175ef55615483
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-04 15:15:33 +0200

    changes

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -24,17 +24,52 @@ const ProductController = () => {
             const product = await ProductService.GetProductById(productId);
             res.status(200).json(product);
         } catch (error) {
-            res.status(400).json(error)
-            console.log(error)
+            res.status(400).json(error);
+            console.log(error);
         }
     }
     const createProduct = async (req: Request, res: Response) => {
-        const data = req.body;
-        if (!data) {
-            return res.status(400).json("No valid data")
+        try {
+            const data = req.body;
+            if (!data) {
+                return res.status(400).json("No valid data")
+            }
+            const product = await ProductService.CreateProduct(data);
+            res.status(200).json(product);
+        } catch (error) {
+            res.status(400).json(error);
+            console.log(error);
         }
-        const product = await ProductService.CreateProduct(data);
-        res.status(200).json(product);
+    }
+    const updateProduct = async (req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+            if (!id) {
+                res.status(400).json("Enter a valid ID")
+            }
+            const data = req.body;
+            if (!data) {
+                return res.status(400).json("No valid data")
+            }
+            const productId = parseInt(id)
+            const product = await ProductService.UpdateProduct(productId, data);
+            res.status(200).json(product);
+        } catch (error) {
+            res.status(400).json(error);
+            console.log(error);
+        }
+    }
+    const deleteProduct = async (req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+            if (!id) {
+                res.status(400).json("Enter a valid ID")
+            }
+            const productId = parseInt(id)
+            await ProductService.DeleteProduct(productId)
+        } catch (error) {
+
+        }
     }
     return {
         getProducts,
