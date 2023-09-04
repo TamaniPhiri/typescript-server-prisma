019508518b7cd2d0011ff6d9405843620cde9439
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-04 15:08:40 +0200

    changes

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -14,8 +14,26 @@ const ProductController=()=>{
             console.log(error)
         }
     }
+    const getProduct=async(req:Request,res:Response)=>{
+        try {
+            const {id}=req.params;
+            const product =await ProductService.GetProductById(id);
+        } catch (error) {
+            
+        }
+    }
+    const createProduct=async(req:Request,res:Response)=>{
+        const data=req.body;
+        if(!data){
+            return res.status(400).json("No valid data")
+        }
+        const product=await ProductService.CreateProduct(data);
+        res.status(200).json(product);
+    }
     return{
-        getProducts
+        getProducts,
+        createProduct,
+
     }
 }
 
