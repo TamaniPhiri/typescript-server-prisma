b338d65979b315d0e774792d29aabe1f7ab793cf
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-04 15:09:15 +0200

    changes

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,11 +1,11 @@
 import ProductService from "../services/ProductService";
 import { Request, Response } from 'express';
 
-const ProductController=()=>{
-    const getProducts=async(req:Request,res:Response)=>{
+const ProductController = () => {
+    const getProducts = async (req: Request, res: Response) => {
         try {
-            const products=await ProductService.GetAllProducts();
-            if(!products){
+            const products = await ProductService.GetAllProducts();
+            if (!products) {
                 return res.status(400).json("No products found")
             }
             res.status(200).json(products);
@@ -14,23 +14,29 @@ const ProductController=()=>{
             console.log(error)
         }
     }
-    const getProduct=async(req:Request,res:Response)=>{
+    const getProduct = async (req: Request, res: Response) => {
         try {
-            const {id}=req.params;
-            const product =await ProductService.GetProductById(id);
+            const { id } = req.params;
+            if (!id) {
+                res.status(400).json("Enter a valid ID")
+            }
+            const productId = parseInt(id)
+            const product = await ProductService.GetProductById(productId);
+            res.status(200).json(product);
         } catch (error) {
-            
+            res.status(400).json(error)
+            console.log(error)
         }
     }
-    const createProduct=async(req:Request,res:Response)=>{
-        const data=req.body;
-        if(!data){
+    const createProduct = async (req: Request, res: Response) => {
+        const data = req.body;
+        if (!data) {
             return res.status(400).json("No valid data")
         }
-        const product=await ProductService.CreateProduct(data);
+        const product = await ProductService.CreateProduct(data);
         res.status(200).json(product);
     }
-    return{
+    return {
         getProducts,
         createProduct,
 
