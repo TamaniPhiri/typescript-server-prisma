e5ef38a99433932afeee7c3db50fb85dd104d39a
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-04 14:40:26 +0200

    changes

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,9 +1,23 @@
 import ProductRepository from "../repositories/ProductRepository";
 
-const ProductService=()=>{
-    const GetAllProducts=async()=>{
-        const products=await ProductRepository.getAllProducts();
-        return products
+const ProductService = () => {
+    const GetAllProducts = async () => {
+        const products = await ProductRepository.getAllProducts();
+        if (products.length <= 0) {
+            throw new Error("No products found");
+        }
+        return products;
+    }
+    const GetProductById = async (id: number) => {
+        const product = await ProductRepository.getProductById(id);
+        if (!product) {
+            throw new Error("No product found");
+        }
+        return product;
+    }
+    return {
+        GetAllProducts,
+        GetProductById
     }
 }
 
