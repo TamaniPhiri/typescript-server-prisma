29b243d3df672fc39989c76f8173f5ee6c51294f
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-04 22:51:49 +0200

    changes

--- src/routers/ProductRouter.ts
+++ src/routers/ProductRouter.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import ProductController from "../controllers/ProductController";
 import cookieJwtAuth from "../middleware/cookieJwtAuth";
 
-const ProductRouter=Router();
+const ProductRouter = Router();
 
-ProductRouter.get('/',cookieJwtAuth,ProductController.getProducts);
-ProductRouter.get('/:id',cookieJwtAuth,ProductController.getProduct);
-ProductRouter.post('/',cookieJwtAuth,ProductController.createProduct);
-ProductRouter.put('/:id',cookieJwtAuth,ProductController.updateProduct);
-ProductRouter.delete('/:id',cookieJwtAuth,ProductController.deleteProduct);
+ProductRouter.get('/', cookieJwtAuth, ProductController.getProducts);
+ProductRouter.get('/:id', cookieJwtAuth, ProductController.getProduct);
+ProductRouter.post('/', cookieJwtAuth, ProductController.createProduct);
+ProductRouter.put('/:id', cookieJwtAuth, ProductController.updateProduct);
+ProductRouter.delete('/:id', cookieJwtAuth, ProductController.deleteProduct);
 
 
 export default ProductRouter;
