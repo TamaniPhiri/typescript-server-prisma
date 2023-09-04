4b1828f2dbd0c94845b5c2ee6a2d2129280faacc
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-04 23:42:35 +0200

    changes

--- src/routers/CustomerRouter.ts
+++ src/routers/CustomerRouter.ts
@@ -3,5 +3,7 @@ import { Router } from "express";
 
 const CustomerRouter=Router();
 CustomerRouter.post('/login',CustomerController.loginCustomer);
-CustomerRouter.post('/register',CustomerController.registerCustomer)
+CustomerRouter.post('/register',CustomerController.registerCustomer);
+CustomerRouter.put('/delete',CustomerController.updateCustomer);
+CustomerRouter.delete('delete',CustomerController.deleteCustomerById);
 export default CustomerRouter;
