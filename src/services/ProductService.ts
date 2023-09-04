616ec1014d2bf6caabe34e28e747279636dbffde
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-04 14:46:07 +0200

    changes

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -15,6 +15,17 @@ const ProductService = () => {
         }
         return product;
     }
+    const CreateProduct=async(data:{ sellerId: number, title: string, description: string, type: string })=>{
+        const product=await ProductRepository.createProduct(data);
+        return product;
+    }
+    const UpdateProduct=async(id:number,data:{ sellerId: number, title: string, description: string, type: string })=>{
+        const updatedProduct=await ProductRepository.updateProductById(id,data)
+        return updatedProduct;
+    }
+    const DeleteProduct=async(id:number)=>{
+        await ProductRepository.deleteProductById(id);
+    }
     return {
         GetAllProducts,
         GetProductById
