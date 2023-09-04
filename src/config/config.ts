5ab0a01ee44dc4f3b78823010cacf7cf58f725fe
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-04 19:02:25 +0200

    auth

---
+++ src/config/config.ts
@@ -0,0 +1,4 @@
+// config.ts
+export const jwtSecret = 'a765s76g!@#$%7sa8f7sctuyg68cfua^&*i3ghifhoa89sai7y5sec89w0%$#';
+export const accessTokenExpiresIn = '60d'; // Access token expires in 15 minutes
+export const refreshTokenExpiresIn = '7d'; // Refresh token expires in 7 days
--- src/services/CustomerService.ts
+++ src/services/CustomerService.ts
@@ -3,11 +3,19 @@ import CustomerRepository from "../repositories/CustomerRepository";
 const CustomerService = () => {
     const RegisterCustomer = async (data: { name: string, email: string, password: string }) => {
         const customer = await CustomerRepository.registerCustomer(data);
-        return customer?.name, customer?.email, customer?.id;
+        return {
+            name: customer?.name,
+            email: customer?.email,
+            id: customer?.id
+        };
     }
     const LoginCustomer = async (email: string, password: string) => {
         const customer = await CustomerRepository.loginCustomer(email, password);
-        return customer?.name, customer?.email, customer?.id;
+        return {
+            name: customer?.name,
+            email: customer?.email,
+            id: customer?.id
+        };
     }
     const UpdateCustomer = async (id: number, data: { name: string, email: string, password: string }) => {
         const customer = await CustomerRepository.updateCustomer(id, data);
